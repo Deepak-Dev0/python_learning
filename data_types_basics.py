cac2a606a39937442bc2dbffde441307222b2fbb
Deepak-Dev0 <Desamaladeepak@gmail.com>
2026-04-12 09:33:53 +0000

    Data types basics

---
+++ data_types_basics.py
@@ -0,0 +1,5 @@
+name = "Deepak Desamala"    #string
+age = 21                    #integer
+height = 5.8                #float
+is_AI_Engineer = True       #bollean
+print(name, age, height, is_AI_Engineer)
