97f8be09dcd29fc4d64f5ac5ec32520756afa592
Deepak-Dev0 <Desamaladeepak@gmail.com>
2026-04-13 18:40:27 +0000

    if-else program

---
+++ Day2/day2.py
@@ -0,0 +1,9 @@
+print("Welcome to AI Decision System")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print(name, "you can apply for AI internship")
+else:
+    print(name, "you are not eligible yet")
