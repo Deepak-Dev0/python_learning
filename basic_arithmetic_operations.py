458bf57deaa9aa5f6bb8dad4891a3a8b3ebaf8f4
Deepak-Dev0 <Desamaladeepak@gmail.com>
2026-04-13 19:44:41 +0000

    learning python

--- basic_arithmetic_operations.py
+++
@@ -1,7 +0,0 @@
-a=10
-b=3
-
-print(a + b)  #addition
-print(a - b)  #subtraction
-print(a * b)  #multiplication
-print(a / b)  #division
--- data_types_basics.py
+++
@@ -1,5 +0,0 @@
-name = "Deepak Desamala"    #string
-age = 21                    #integer
-height = 5.8                #float
-is_AI_Engineer = True       #bollean
-print(name, age, height, is_AI_Engineer)
