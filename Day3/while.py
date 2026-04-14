87c21e5e1ea3cf2d1f1decc192d22ee6f8326225
Deepak-Dev0 <Desamaladeepak@gmail.com>
2026-04-14 18:14:12 +0000

    WHILE LOOP

---
+++ Day3/while.py
@@ -0,0 +1,11 @@
+""" 
+while loop is a conditional loop, it iterate until condition is
+True OR Not False.
+
+variable value must update, if not loop will continue infinte times
+"""
+
+i=1
+while i<=5:
+	print(i)
+	i += 1
