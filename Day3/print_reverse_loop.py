8920676f93a4a78a0e40266339d435981dfb2a9e
Deepak-Dev0 <Desamaladeepak@gmail.com>
2026-04-14 18:52:37 +0000

    Reverse loop printing

---
+++ Day3/print_reverse_loop.py
@@ -0,0 +1,12 @@
+"""
+for i in range(  10  ,  0  ,   -1   )
+for i in range(start , end , reverse)
+reverse means it not trun the back to front ,it just step backward
+means 1 forward, -1 backward
+
+for loop by default moves forward ,-1 make it to backward 
+"""
+
+for i in range(10, 0, -1):
+#10-1 -->9, 9-1-->8........until end =0
+    print(i)
