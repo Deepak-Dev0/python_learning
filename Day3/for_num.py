8db30063b195f3d1197c44b4aeb3c74e0ad8c468
Deepak-Dev0 <Desamaladeepak@gmail.com>
2026-04-14 17:43:17 +0000

    DEFINE START AND END POINTS IN FOR LOOP

--- Day3/for_num.py
+++ Day3/for_num.py
@@ -1,2 +1,8 @@
+#for i in range( 5 )
+#for i in range(end)
+#end = num-1
+#end = 5-1 --> 4
+#numbering starts from zero -->  0 1 2 3 4
+
 for i in range(5):
 	print(i)
---
+++ Day3/for_start_end.py
@@ -0,0 +1,7 @@
+#for i in range(  1   ,  6)
+#for i in range(start , end)
+#end =num-1
+#end =6-1 --> 5
+
+for i in range (1,6):
+	print(i)
