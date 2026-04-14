718b22c32df7fe4c466d41a9c096971690117c47
Deepak-Dev0 <Desamaladeepak@gmail.com>
2026-04-14 18:05:16 +0000

    STEP FORWARD USING FOR LOOP

---
+++ Day3/for_step.py
@@ -0,0 +1,8 @@
+#for i in range (  1  , 10 ,  2  )
+#for i in range (start, end, step)
+#step means forward a step
+#start =1 ,end =num-1 --> 9 ,step =2
+#output --> 1 3 5 7 9
+
+for i in range (1,10,2):
+	print(i)
