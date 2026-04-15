0634d9d6e691a50608b316a3097617ec81f1ed12
Deepak-Dev0 <Desamaladeepak@gmail.com>
2026-04-15 12:53:13 +0000

    Access elements using there index

---
+++ Day4/access_list.py
@@ -0,0 +1,15 @@
+"""
+we can access perticular elements in a list by using index
+"""
+
+num = [10,20,30]
+print(num[0])   #10
+"""
+we represent index using "variable_name[index]"
+
+varible_name --> where list is stored  #in this program it is "num"
+   []        --> square brackets mandatory
+ index       --> position of element want to access
+index always starts from zero, so index 0 --> 10,index 1 --20.....
+"""
+print(num[2]) #30
