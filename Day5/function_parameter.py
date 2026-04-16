1062fd5756cdd34d96c7ccb311b9be14c5a5d522
Deepak-Dev0 <Desamaladeepak@gmail.com>
2026-04-16 18:05:46 +0000

    created a function  with parameters

---
+++ Day5/function_parameter.py
@@ -0,0 +1,8 @@
+def greet(name):
+	"""
+we use "name" as a parameter,it can be changed as you like
+parameter means a variable that receives input when function is called
+"""
+	print("hello",name) #here hello print along with input we give
+greet("Deepak") #Deepak is passed to function,function store it in "name"
+greet("AI world") #AI world is passed to function,it store it in "name"
