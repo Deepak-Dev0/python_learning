5df52b0a9bf5e6567a5f7ce3db598c41a534f0a9
Deepak-Dev0 <Desamaladeepak@gmail.com>
2026-04-16 18:19:18 +0000

    created a function with return value

---
+++ Day5/function_return.py
@@ -0,0 +1,12 @@
+def add(a,b):
+	return a+b
+#return --> sending result back from function
+sum=add(10,20)
+"""
+*sum is a variable used to store the result that come from the call function
+*add(10,20) calls the main function and pass parameters 10 and 20
+*main function store values in varibles a=10, b=20
+*now both values calculated a+b, return sends the result to function
+*then print the sum
+"""
+print(sum)
