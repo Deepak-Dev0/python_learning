56407b388ef38d9bbe3dbc6387a84cd76f9d8866
Deepak-Dev0 <Desamaladeepak@gmail.com>
2026-04-16 17:52:21 +0000

    Basic function creation

---
+++ Day5/Basic_function.py
@@ -0,0 +1,12 @@
+"""
+Functin is a block of code that performs a task and can be reused as many
+time as we want.
+"""
+#"def" is the keyword used to create a function.
+def greet():
+#greet --> function name you ,can change it
+	print("Welcome to my world")
+
+greet() #it pritn output ,it is a call for the function to respond
+greet()
+greet()
